7cfe129578409de769d0c01b9586278cdfc99509
Liz Izhikevich <lizhikevich@gmail.com>
2020-03-12 19:00:12 +0000

    added mongo fingerprint

--- fingerprint.go
+++ fingerprint.go
@@ -3,9 +3,54 @@ package main
 import (
     "net/http"
     "net/http/httputil"
+    "strings"
+    "gopkg.in/mgo.v2/bson"
+    "encoding/binary"
 )
 
+func getOpQuery(collname string, query []byte) ([]byte) {
+
+        const MSGHEADER_LEN = 16
+        const OP_QUERY = 2004
+        flagslen := 4
+        collname_len := len(collname) + 1
+        nskiplen := 4
+        nretlen := 4
+        qlen := len(query)
+        msglen := MSGHEADER_LEN + flagslen + collname_len + nskiplen + nretlen + qlen
+        out := make([]byte, msglen)
+        // msg header
+        binary.LittleEndian.PutUint32(out[0:], uint32(msglen))
+        binary.LittleEndian.PutUint32(out[12:], OP_QUERY)
+        // query msg
+        idx := MSGHEADER_LEN + flagslen
+        copy(out[idx:idx+collname_len], []byte(collname))
+        idx += collname_len + nskiplen
+        binary.LittleEndian.PutUint32(out[idx:idx+nretlen], 1)
+        idx += nretlen
+        copy(out[idx:idx+qlen], query)
+        return out
+}
+
 func getData( dst string ) []byte {
+
+    query, _ := bson.Marshal(bson.M{ "isMaster": 1 })
+    query_msg := getOpQuery("admin.$cmd", query)
+    return query_msg
+
+}
+
+func fingerprintResponse( data string ) string {
+
+    if strings.Contains( data, "maxBsonObjectSize" ) {
+        return "Mongo"
+    } else {
+        return ""
+    }
+
+}
+
+func getDataHTTP( dst string ) []byte {
 
         req, _ := http.NewRequest("GET","/",nil)
         req.Host =  dst
@@ -15,4 +60,14 @@ func getData( dst string ) []byte {
         req.Header.Set("Accept-Encoding","gzip")
         data, _ := httputil.DumpRequest(req, false)
 	return data
+}
+
+func fingerprintResponseHTTP( data string ) string {
+
+    if strings.Contains( data, "HTTP" ) {
+        return "HTTP"
+    } else {
+        return ""
+    }
+
 }
