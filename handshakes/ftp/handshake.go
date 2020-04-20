90c9cc44b822a478ae12df8d7227274fea77385a
Liz <lizhikev@stanford.edu>
2020-04-20 17:57:11 +0000

    hyper optimized toLower func for fingerprinting

--- handshakes/ftp/handshake.go
+++ handshakes/ftp/handshake.go
@@ -2,7 +2,9 @@ package ftp
 
 import (
 	"lzr"
+	"unicode"
 	"strings"
+	"unicode/utf8"
 )
 
 // Handshake implements the lzr.Handshake interface
@@ -15,7 +17,7 @@ func (h *HandshakeMod) GetData( dst string ) []byte {
 }
 
 func (h *HandshakeMod) Verify( data string ) string {
-	if strings.Contains( strings.ToLower(data), "ftp" ) {
+	if strings.Contains( ToLower(data), "ftp" ) {
 		return "ftp"
 	}
     return ""
@@ -24,5 +26,35 @@ func (h *HandshakeMod) Verify( data string ) string {
 func RegisterHandshake() {
 	var h HandshakeMod
 	lzr.AddHandshake( "ftp", &h )
+}
+
+//more efficient string toLower
+//https://github.com/golang/go/issues/17859
+
+func ToLower(s string) string {
+	if s == "" { // quick return for empty strings
+		return s
+	}
+	if isASCII(s) { // optimize for ascii condition
+		b := make([]byte, len(s))
+		for i, c := range s {
+			if c >= 'A' && c <= 'Z' {
+				c += 32
+			}
+			b[i] = byte(c)
+		}
+		return string(b)
+	}
+	//should i even call ToLower for non-ascii??
+	return strings.Map(unicode.ToLower, s)
+}
+
+func isASCII(s string) bool {
+	for i := 0; i < len(s); i++ {
+		if s[i] >= utf8.RuneSelf {
+			return false
+		}
+	}
+	return true
 }
 
