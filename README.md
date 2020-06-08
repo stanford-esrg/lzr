16745b45c74bd4a29783adcec35f2faccbb102ed
Liz <lizhikev@stanford.edu>
2020-06-08 19:46:03 +0000

    fixed hyperacktive edge case. debug ending is still turned on

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # LZR
 LZR: Accurately and Efficiently Identifying Service Liveness and Protocols in Internet-Wide Scans
+
+###HyperACKtive
+Note: If a host responds both on the expected port and on the random ephemeral port, whichever response comes first will dictate whether the host is marked as HyperACKtive. The expected port is contacted first, so unless there is some congestion which causes the packets to be delivered out of order, then the expected port is expected to answer first.  
--- handle_expired.go
+++ handle_expired.go
@@ -20,8 +20,7 @@ func handleExpired( opts *options, packet * packet_metadata, ipMeta * pState,
 	handshakeNum := ipMeta.getHandshake( packet )
 	//if we are all not trying anymore handshakes, so sad. 
 	//if ( packet.ExpectedRToLZR == SYN_ACK ||
-	if (
-		packet.HyperACKtive  || (handshakeNum >= (len( opts.Handshakes ) - 1)) ){
+	if ( packet.HyperACKtive  || (handshakeNum >= (len( opts.Handshakes ) - 1)) ){
 
 		packet.syncHandshakeNum( handshakeNum )
 
@@ -32,16 +31,12 @@ func handleExpired( opts *options, packet * packet_metadata, ipMeta * pState,
 
 		//remove from state, we are done now
 		packet = ipMeta.remove( packet )
+		if HyperACKtiveFiltering() {
+			packet.HyperACKtive = true
+			packet = ipMeta.remove( packet )
+		}
 	} else { // lets try another handshake
 
-		//lets also filter for cananda-like things
-		if ( handshakeNum == 0 &&  HyperACKtiveFiltering() ) {
-
-			highPortPacket := createFilterPacket( packet )
-			sendOffSyn( highPortPacket, ipMeta, timeoutQueue )
-			ipMeta.FinishProcessing( highPortPacket )
-
-		}
 
 		//record all succesful fingerprints if forcing all handshakes
 		if ForceAllHandshakes() && len(packet.Data) > 0 {
@@ -50,27 +45,36 @@ func handleExpired( opts *options, packet * packet_metadata, ipMeta * pState,
 		}
 
 		packet.updatePacketFlow()
-        ipMeta.incHandshake( packet )
+		ipMeta.incHandshake( packet )
 		sendOffSyn( packet, ipMeta, timeoutQueue )
+
+
+		//lets also filter for cananda-like things
+		if ( handshakeNum == 0 &&  HyperACKtiveFiltering() ) {
+			highPortPacket := createFilterPacket( packet )
+			sendOffSyn( highPortPacket, ipMeta, timeoutQueue )
+			ipMeta.FinishProcessing( highPortPacket )
+		}
+
 	}
 
 }
 
 func sendOffSyn(packet * packet_metadata, ipMeta * pState,
-    timeoutQueue chan *packet_metadata ) {
+	timeoutQueue chan *packet_metadata ) {
 
-        packet.updateResponse( SYN_ACK )
-        packet.updateTimestamp()
-        ipMeta.update( packet )
-        syn := constructSYN( packet )
-        // send SYN packet if so and start the whole process again
-        err := handle.WritePacketData(syn)
-        if err != nil {
-            panic(err)
+		packet.updateResponse( SYN_ACK )
+		packet.updateTimestamp()
+		ipMeta.update( packet )
+		syn := constructSYN( packet )
+		// send SYN packet if so and start the whole process again
+		err := handle.WritePacketData(syn)
+		if err != nil {
+			panic(err)
 
-        }
-        //wait for a s/a
-        packet.updateTimestamp()
-        timeoutQueue <- packet
+		}
+		//wait for a s/a
+		packet.updateTimestamp()
+		timeoutQueue <- packet
 
 }
