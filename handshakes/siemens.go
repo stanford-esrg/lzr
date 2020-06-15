0ce2e4ec8631264c7fb2d8eb9d0bf852715ae857
Liz <lizhikev@stanford.edu>
2020-06-15 00:12:53 +0000

    s7

---
+++ handshakes/siemens.go
@@ -0,0 +1,8 @@
+package handshakes
+
+import "lzr/handshakes/siemens"
+
+func init() {
+	siemens.RegisterHandshake()
+}
+
---
+++ handshakes/siemens/handshake.go
@@ -0,0 +1,37 @@
+package siemens
+
+import (
+	"lzr"
+	"encoding/hex"
+)
+
+// Handshake implements the lzr.Handshake interface
+type HandshakeMod struct {
+}
+
+func (h *HandshakeMod) GetData( dst string ) []byte {
+	datas := "0300001611e00000000400c1020100c2020200c0010a"
+	data, _:= hex.DecodeString(datas)
+	//data := []byte("\x00\x6d\x00\x00\x01\x00\x00\x00\x01\x38\x01\x2c \x00\x6d\x00\x00\x01\x00\x00\x00\x01\x38\x01\x2c")
+    return data
+}
+
+func (h *HandshakeMod) Verify( data string ) string {
+
+    datab := []byte(data)
+    if len(datab) < 6 {
+        return ""
+    }
+	size := datab[4]
+	pduType := datab[5]
+	if int(size) + 1 != len(datab[4:]) || pduType != 0xd0 {
+		return ""
+	}
+	return "siemens"
+}
+
+func RegisterHandshake() {
+	var h HandshakeMod
+	lzr.AddHandshake( "siemens", &h )
+}
+
