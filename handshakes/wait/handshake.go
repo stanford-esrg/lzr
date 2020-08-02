98ffd110a55b721dd6cf2aff9eb9c1d5b7055d86
Liz <lizhikev@stanford.edu>
2020-08-02 19:56:49 +0000

    wait module

---
+++ handshakes/wait/handshake.go
@@ -0,0 +1,26 @@
+package wait
+
+import (
+	"lzr"
+)
+
+// Handshake implements the lzr.Handshake interface
+type HandshakeMod struct {
+}
+
+//server first protocol
+func (h *HandshakeMod) GetData( dst string ) []byte {
+	data := []byte("")
+	return data
+}
+
+func (h *HandshakeMod) Verify( data string ) string {
+	return ""
+}
+
+
+func RegisterHandshake() {
+	var h HandshakeMod
+	lzr.AddHandshake( "wait", &h )
+}
+
