01ec8b74c76ee33cab902f6a2ddf12bfb525847b
Liz <lizhikev@stanford.edu>
2020-10-23 02:54:16 +0000

    added apache 2.0 license info

--- concurrentMap.go
+++ concurrentMap.go
@@ -1,6 +1,23 @@
-// includes code from 
-// https://github.com/orcaman/concurrent-map/blob/master/concurrent_map.go
-// which is licensed under the MIT license (Copyright (c) 2014 streamrail)
+/*
+Copyright 2020 The Board of Trustees of The Leland Stanford Junior University
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+This file also includes code from 
+https://github.com/orcaman/concurrent-map/blob/master/concurrent_map.go
+which is licensed under the MIT license (Copyright (c) 2014 streamrail)
+*/
+
 
 package lzr
 
--- handle_ack.go
+++ handle_ack.go
@@ -1,3 +1,18 @@
+/*
+Copyright 2020 The Board of Trustees of The Leland Stanford Junior University
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package lzr
 
 import (
@@ -7,11 +22,10 @@ import (
 
 
 
-func SendAck( opts *options, synack  *packet_metadata, ipMeta * pState, 
+func SendAck( opts *options, synack  *packet_metadata, ipMeta * pState,
 timeoutQueue  chan *packet_metadata, retransmitQueue chan *packet_metadata, writingQueue  chan packet_metadata ) {
 
 
-	//TODO: check that ip_metadata contains what we want (saddr,seq,ack,window)
 	if synack.windowZero() {
 		//not a real s/a
 		writingQueue <- *synack
