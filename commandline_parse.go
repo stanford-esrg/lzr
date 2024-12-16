7edcf412d6f88cc32cd3a5183835d7333d73d679
daowilliam310 <daowilliam310@gmail.com>
2024-12-16 01:04:51 -0800

    Revert "Add dry run functionality"
    This reverts commit d657c04c8bfcc29811a758d59381f42a30eeb327.

--- commandline_parse.go
+++ commandline_parse.go
@@ -15,112 +15,112 @@ limitations under the License.
 */
 
 package lzr
-
 import (
-	"flag"
-	"fmt"
-	"os"
-	"strings"
-	"time"
+  "flag"
+  "fmt"
+  "os"
+  "time"
+  "strings"
 )
 
 var (
-	filename               *string
-	sendSYNs               *bool
-	sourceIP               *string
-	device                 *string
-	mac                    *string
-	debug                  *bool
-	haf                    *int
-	pushDOnly              *bool
-	forceAllHandshakes     *bool
-	feedZGrab              *bool
-	workers                *int
-	timeout                *int
-	retransmitSec          *int
-	retransmitNum          *int
-	cpuprofile             *string
-	memprofile             *string
-	handshake              *string
-	priorityFingerprint    *string
-	priorityFingerprintArr []string
-	handshakeArr           []string
-	recordOnlyData         *bool
-	dryrun                 *bool
+
+	filename				*string
+	sendSYNs				*bool
+	sourceIP				*string
+	device					*string
+	mac						*string
+	debug					*bool
+	haf						*int
+	pushDOnly				*bool
+	forceAllHandshakes		*bool
+	feedZGrab				*bool
+	workers					*int
+	timeout					*int
+	retransmitSec			*int
+	retransmitNum			*int
+	cpuprofile				*string
+	memprofile				*string
+	handshake				*string
+	priorityFingerprint		*string
+	priorityFingerprintArr	[]string
+	handshakeArr			[]string
+	recordOnlyData			*bool
 )
 
 type options struct {
-	Filename            string
-	SendSYNs            bool
-	SourceIP            string
-	Device              string
-	Mac                 string
-	Debug               bool
-	Haf                 int
-	PushDOnly           bool
-	ForceAllHandshakes  bool
-	FeedZGrab           bool
-	Workers             int
-	Timeout             int
-	RetransmitSec       int
-	RetransmitNum       int
-	CPUProfile          string
-	MemProfile          string
-	Handshakes          []string
-	PriorityFingerprint []string
-	RecordOnlyData      bool
-	Dryrun              bool
+
+	Filename			string
+	SendSYNs			bool
+	SourceIP			string
+	Device				string
+	Mac					string
+	Debug				bool
+	Haf					int
+	PushDOnly			bool
+	ForceAllHandshakes	bool
+	FeedZGrab			bool
+	Workers				int
+	Timeout				int
+	RetransmitSec		int
+	RetransmitNum		int
+	CPUProfile			string
+	MemProfile			string
+	Handshakes			[]string
+	PriorityFingerprint	[]string
+	RecordOnlyData		bool
 }
+
 
 // Basic flag declarations are available for string, integer, and boolean options.
 func init() {
-	fname := "default_" + string(time.Now().Format("20060102150405")) + ".json"
-	filename = flag.String("f", fname, "json results output file name, use '-' for standard output")
-	sendSYNs = flag.Bool("sendSYNs", false, "will read input from stdin containing a newline-delimited list of ip:port")
-	sourceIP = flag.String("sourceIP", "", "source IP to send syn packets with (if using sendSYNs flag)")
-	device = flag.String("sendInterface", "ens8", "network interface to send packets on")
-	mac = flag.String("gatewayMac", "", "gateway Mac Address in format xx:xx:xx:xx:xx:xx")
-	debug = flag.Bool("d", false, "debug printing on")
-	haf = flag.Int("haf", 0, "number of random ephemeral probes to send to filter ACKing firewalls")
-	pushDOnly = flag.Bool("pushDataOnly", false, "Don't attach data to ack but rather to push only")
-	forceAllHandshakes = flag.Bool("forceAllHandshakes", false, "Complete all handshakes even if data is returned early on. This also turns off HyperACKtive filtering.")
-	feedZGrab = flag.Bool("feedZGrab", false, "send to zgrab ip and fingerprint")
-	workers = flag.Int("w", 1, "number of worker threads for each channel")
-	timeout = flag.Int("t", 5, "number of seconds to wait in timeout queue for last retransmission")
-	retransmitSec = flag.Int("rt", 1, "number of seconds until re-transmitting packet")
-	retransmitNum = flag.Int("rn", 1, "number of data packets to re-transmit")
-	cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
-	memprofile = flag.String("memprofile", "", "write memory profile to this file")
-	handshake = flag.String("handshakes", "http", "handshakes to scan with")
-	priorityFingerprint = flag.String("priorityFingerprint", "", "fingerprint to prioritize when multiple match")
-	recordOnlyData = flag.Bool("onlyDataRecord", false, "record to file only services that send back data")
-	dryrun = flag.Bool("dryrun", false, "will take dryrun output from ZMap (enables sendSYNs)")
+  fname := "default_"+string(time.Now().Format("20060102150405"))+".json"
+  filename = flag.String("f", fname , "json results output file name, use '-' for standard output")
+  sendSYNs = flag.Bool("sendSYNs", false , "will read input from stdin containing a newline-delimited list of ip:port")
+  sourceIP = flag.String("sourceIP", "" , "source IP to send syn packets with (if using sendSYNs flag)")
+  device = flag.String("sendInterface", "ens8" , "network interface to send packets on")
+  mac = flag.String("gatewayMac", "" , "gateway Mac Address in format xx:xx:xx:xx:xx:xx")
+  debug = flag.Bool("d", false, "debug printing on")
+  haf = flag.Int("haf", 0, "number of random ephemeral probes to send to filter ACKing firewalls")
+  pushDOnly = flag.Bool("pushDataOnly", false, "Don't attach data to ack but rather to push only")
+  forceAllHandshakes = flag.Bool("forceAllHandshakes", false, "Complete all handshakes even if data is returned early on. This also turns off HyperACKtive filtering.")
+  feedZGrab = flag.Bool("feedZGrab", false, "send to zgrab ip and fingerprint")
+  workers = flag.Int("w", 1 , "number of worker threads for each channel")
+  timeout = flag.Int("t", 5, "number of seconds to wait in timeout queue for last retransmission")
+  retransmitSec = flag.Int("rt", 1 , "number of seconds until re-transmitting packet")
+  retransmitNum = flag.Int("rn", 1 , "number of data packets to re-transmit")
+  cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
+  memprofile = flag.String("memprofile", "", "write memory profile to this file")
+  handshake = flag.String("handshakes", "http" , "handshakes to scan with")
+  priorityFingerprint = flag.String("priorityFingerprint", "" , "fingerprint to prioritize when multiple match")
+  recordOnlyData = flag.Bool("onlyDataRecord", false, "record to file only services that send back data")
 }
 
-func checkAndParse(handshake *string, optHandshakes *[]string) ([]string, bool) {
+
+func checkAndParse( handshake *string, optHandshakes *[]string ) ( []string, bool ) {
 
 	if *handshake == "" {
 		return nil, true
 	}
 
-	if !strings.Contains(*handshake, ",") {
+	if !strings.Contains( *handshake, ",")	{
 
 		_, ok := GetHandshake(*handshake)
 
 		if !ok {
-			fmt.Fprintln(os.Stderr, "--Handshake not found:", *handshake)
-			return nil, false
+			fmt.Fprintln(os.Stderr,"--Handshake not found:", *handshake)
+			return nil,false
 		}
 
 		(*optHandshakes)[0] = *handshake
 	} else {
 		i := 0
-		for _, h := range strings.Split(*handshake, ",") {
+		for _, h := range strings.Split( *handshake, "," ) {
 
 			_, ok := GetHandshake(h)
 			if !ok {
-				fmt.Fprintln(os.Stderr, "--Handshake not found:", h)
-				return nil, false
+				fmt.Fprintln(os.Stderr,"--Handshake not found:", h)
+				return nil,false
 			}
 
 			(*optHandshakes)[i] = h
@@ -131,38 +131,38 @@ func checkAndParse(handshake *string, optHandshakes *[]string) ([]string, bool) 
 
 }
 
-func Parse() (*options, bool) {
+
+func Parse() (*options,bool) {
 
 	flag.Parse()
 	opt := &options{
-		Filename:            *filename,
-		SendSYNs:            *sendSYNs,
-		Debug:               *debug,
-		Device:              *device,
-		Mac:                 *mac,
-		Haf:                 *haf,
-		FeedZGrab:           *feedZGrab,
-		PushDOnly:           *pushDOnly,
-		ForceAllHandshakes:  *forceAllHandshakes,
-		Workers:             *workers,
-		Timeout:             *timeout,
-		RetransmitSec:       *retransmitSec,
-		RetransmitNum:       *retransmitNum,
-		CPUProfile:          *cpuprofile,
-		MemProfile:          *memprofile,
-		Handshakes:          make([]string, strings.Count(*handshake, ",")+1),
-		PriorityFingerprint: make([]string, strings.Count(*priorityFingerprint, ",")+1),
-		RecordOnlyData:      *recordOnlyData,
-		Dryrun:              *dryrun,
+		Filename: *filename,
+		SendSYNs: *sendSYNs,
+		Debug: *debug,
+		Device: *device,
+		Mac: *mac,
+		Haf: *haf,
+		FeedZGrab: *feedZGrab,
+		PushDOnly: *pushDOnly,
+		ForceAllHandshakes: *forceAllHandshakes,
+		Workers: *workers,
+		Timeout: *timeout,
+		RetransmitSec: *retransmitSec,
+		RetransmitNum: *retransmitNum,
+		CPUProfile: *cpuprofile,
+		MemProfile: *memprofile,
+		Handshakes: make([]string, strings.Count(*handshake,",")+1),
+		PriorityFingerprint: make([]string, strings.Count(*priorityFingerprint,",")+1),
+		RecordOnlyData: *recordOnlyData,
 	}
 
 	success := false
-	handshakeArr, success = checkAndParse(handshake, &(opt.Handshakes))
+	handshakeArr, success = checkAndParse( handshake, &(opt.Handshakes) )
 	if !success {
 		return nil, false
 	}
 
-	priorityFingerprintArr, success = checkAndParse(priorityFingerprint, &(opt.PriorityFingerprint))
+	priorityFingerprintArr, success = checkAndParse( priorityFingerprint, &(opt.PriorityFingerprint) )
 	if !success {
 		return nil, false
 	}
@@ -171,57 +171,53 @@ func Parse() (*options, bool) {
 		*haf = 0
 	}
 
-	fmt.Fprintln(os.Stderr, "++Writing results to file:", *filename)
-	fmt.Fprintln(os.Stderr, "++Handshakes:", *handshake)
-	if *dryrun {
-		fmt.Fprintln(os.Stderr, "++Reading from dryrun")
-		*sendSYNs = true
-	}
+	fmt.Fprintln(os.Stderr,"++Writing results to file:", *filename)
+	fmt.Fprintln(os.Stderr,"++Handshakes:", *handshake)
 	if *sendSYNs {
-		fmt.Fprintln(os.Stderr, "++Sending SYNs")
+		fmt.Fprintln(os.Stderr,"++Sending SYNs")
 	}
 	if *sourceIP != "" {
-		fmt.Fprintln(os.Stderr, "++Using SourceIP:", *sendSYNs)
+		fmt.Fprintln(os.Stderr,"++Using SourceIP:", *sendSYNs)
 	}
 	if *device != "ens8" {
-		fmt.Fprintln(os.Stderr, "++Using Sending Interface:", *device)
+		fmt.Fprintln(os.Stderr,"++Using Sending Interface:", *device)
 	}
 	if *mac != "" {
-		fmt.Fprintln(os.Stderr, "++Using Gateway Mac:", *mac)
+		fmt.Fprintln(os.Stderr,"++Using Gateway Mac:", *mac)
 	}
 	if *priorityFingerprint != "" {
-		fmt.Fprintln(os.Stderr, "++Prioritizing Fingerprints:", *priorityFingerprint)
+		fmt.Fprintln(os.Stderr,"++Prioritizing Fingerprints:", *priorityFingerprint)
 	}
 	if *memprofile != "" {
-		fmt.Fprintln(os.Stderr, "++Writing memprofile to file:", *memprofile)
+		fmt.Fprintln(os.Stderr,"++Writing memprofile to file:", *memprofile)
 	}
 	if *cpuprofile != "" {
-		fmt.Fprintln(os.Stderr, "++Writing cpuprofile to file:", *cpuprofile)
+		fmt.Fprintln(os.Stderr,"++Writing cpuprofile to file:", *cpuprofile)
 	}
 	if *debug {
-		fmt.Fprintln(os.Stderr, "++Debug turned on")
+		fmt.Fprintln(os.Stderr,"++Debug turned on")
 	}
 	if *haf > 0 {
-		fmt.Fprintln(os.Stderr, "++Sending ", *haf, " number of filtering packets")
+		fmt.Fprintln(os.Stderr,"++Sending ",*haf, " number of filtering packets")
 	}
 	if *feedZGrab {
-		fmt.Fprintln(os.Stderr, "++Feeding ZGrab with fingerprints")
+		fmt.Fprintln(os.Stderr,"++Feeding ZGrab with fingerprints")
 	}
 	if *pushDOnly {
-		fmt.Fprintln(os.Stderr, "++Sending Data only with Push Flag (not in ack)")
+		fmt.Fprintln(os.Stderr,"++Sending Data only with Push Flag (not in ack)")
 	}
 	if *forceAllHandshakes {
-		fmt.Fprintln(os.Stderr, "++Force completing all handshakes")
+		fmt.Fprintln(os.Stderr,"++Force completing all handshakes")
 	}
 	if *recordOnlyData {
-		fmt.Fprintln(os.Stderr, "++Recording to file only services that return data")
+		fmt.Fprintln(os.Stderr,"++Recording to file only services that return data")
 	}
-	fmt.Fprintln(os.Stderr, "++Worker threads:", *workers)
-	fmt.Fprintln(os.Stderr, "++Timeout Interval (s):", *timeout)
-	fmt.Fprintln(os.Stderr, "++Retransmit Interval (s):", *retransmitSec)
-	fmt.Fprintln(os.Stderr, "++Number of Retransmitions:", *retransmitNum)
+	fmt.Fprintln(os.Stderr,"++Worker threads:", *workers)
+	fmt.Fprintln(os.Stderr,"++Timeout Interval (s):", *timeout)
+	fmt.Fprintln(os.Stderr,"++Retransmit Interval (s):", *retransmitSec)
+	fmt.Fprintln(os.Stderr,"++Number of Retransmitions:", *retransmitNum)
 	//fmt.Fprintln(os.Stderr,"port:", *port)
-	return opt, true
+	return opt,true
 }
 
 func DebugOn() bool {
@@ -244,10 +240,6 @@ func ReadZMap() bool {
 	return *sendSYNs != true
 }
 
-func DryRun() bool {
-	return *dryrun
-}
-
 func getNumFilters() int {
 	return *haf
 }
@@ -257,7 +249,7 @@ func getSourceIP() string {
 }
 
 func getDevice() string {
-	return *device
+    return *device
 }
 
 func getHostMacAddr() string {
@@ -272,7 +264,7 @@ func ForceAllHandshakes() bool {
 	return *forceAllHandshakes
 }
 
-func GetAllHandshakes() []string {
+func GetAllHandshakes()  []string {
 
 	if priorityFingerprintArr != nil {
 		return priorityFingerprintArr
@@ -281,5 +273,6 @@ func GetAllHandshakes() []string {
 }
 
 func NumHandshakes() int {
-	return len(handshakeArr)
+    return len(handshakeArr)
 }
+
